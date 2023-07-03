094cde95ea572b80948fd140ff0288f35ba0e9a5
Ibrahim Isber <ibrahimisber1997@gmail.com>
2023-07-03 11:06:19 +0300

    color and 13 event

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let imgBox =document.getElementById("imgBox");
 let qrImage =document.getElementById("qrImage");
 let qrText =document.getElementById("qrText");
 
+qrText.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      generateQR();
+    }
+  });
+
 function generateQR(){
     if(qrText.value.length > 0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
